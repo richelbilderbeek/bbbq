14f74ba8962c6d6d02046cef5cc3e34f5f067f9b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-30 20:36:18 +0100

    Do not call NetMHC2pan yet

--- tests/testthat/test-create_tmh_fasta.R
+++ tests/testthat/test-create_tmh_fasta.R
@@ -1,8 +1,8 @@
 context("test-create_tmh_fasta")
 
 test_that("use", {
-  #tmh_prot_filename <- tempfile(fileext = ".fasta")
-  tmh_prot_filename <- "/home/richel/tmh_prot.fasta"
+  skip("WIP")
+  tmh_prot_filename <- tempfile(fileext = ".fasta")
   tmhprot::create_tmh_prot_file(
     proteome_filename = get_bbbq_path(
       "UP000001584_83332_tr_I6WYT7_I6WYT7_MYCTU.fasta"
@@ -14,20 +14,4 @@ test_that("use", {
   )
   lines <- readLines(tmh_prot_filename)
   expect_equal(length(lines), 16)
-})
-
-test_that("full use", {
-
-  if (1 == 2) {
-    df_all <- read.csv(file = "~/bbbq/tbc_netmhc2pan_bindings.csv")
-    # Strong binders
-    df <- df_all[df_all$Rank <= 5.0, ]
-    # All we need
-    df_sub <-df[, c("Pos", "Peptide", "ID", "Rank")]
-    utils::write.csv(
-      x = df_sub,
-      file = "~/bbbq/tbc_netmhc2pan_bindings_strong.csv",
-      row.names = FALSE
-    )
-  }
 })
