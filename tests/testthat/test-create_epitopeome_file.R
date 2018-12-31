dd7698a0467029fe982491d6acd74142465b3bb4
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-31 15:22:23 +0100

    Cleanup

--- tests/testthat/test-create_epitopeome_file.R
+++ tests/testthat/test-create_epitopeome_file.R
@@ -3,16 +3,10 @@ context("test-create_epitopeome_file")
 test_that("use", {
 
   if (1 == 2) {
-
-    tmhs_filename <- "~/bbbq/tbc_tmhmm.txt"
-    netmhc2pan_filename <- "~/bbbq/tbc_netmhc2pan_bindings_strong.csv"
-    testit::assert(file.exists(tmhs_filename))
-    testit::assert(file.exists(netmhc2pan_filename))
-
-    epitopeome_filename <- "~/tbc_epitopeome.fasta"
-    epitopeome:::create_epitopeome_file_cpp(
-      tmhs_filename = tmhs_filename,
-      netmhc2pan_filename = netmhc2pan_filename,
+    fasta_filename <- get_bbbq_path("short.fasta")
+    epitopeome_filename <- tempfile()
+    epitopeome:::create_epitopeome_file(
+      fasta_filename = fasta_filename,
       epitopeome_filename = epitopeome_filename
     )
     expect_true(file.exists(epitopeome_filename))
--- tests/testthat/test-get_protein_lengths.R
+++
@@ -1,9 +0,0 @@
-context("test-get_protein_lengths")
-
-test_that("use", {
-  df <- get_protein_lengths()
-  expect_true(nrow(df) > 1)
-  expect_true("name" %in% colnames(df))
-  expect_true("n_aa" %in% colnames(df))
-
-})
