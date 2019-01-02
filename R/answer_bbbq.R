e22cf0f5d53c8a3fddffbae4cecfd9d34a0c6760
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-02 13:20:23 +0100

    Suggest epitopeome

---
+++ R/answer_bbbq.R
@@ -0,0 +1,16 @@
+#' Answer the BBBQ
+#' @inheritParams default_params_doc
+answer_bbbq <- function(
+  fasta_filename
+) {
+  testit::assert(file.exists(fasta_filename))
+  epitopeome::create_epitopeome(
+    fasta_filename = fasta_filename,
+    alleles = alleles,
+    binding_strength_threshold = binding_strength_threshold,
+    tmhmm_filename = tmhmm_filename,
+    netmhc2pan_filename = netmhc2pan_filename,
+    epitopeome_filename = epitopeome_filename
+  )
+  readLines(epitopeome_filename)
+}
