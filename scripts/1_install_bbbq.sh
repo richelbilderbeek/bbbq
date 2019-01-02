7f91e6a03d2c5a4f8ee959869fa38b446f836ede
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-02 16:08:11 +0100

    Add Peregrine scripts

---
+++ scripts/1_install_bbbq.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script to install BBBQ and its dependencies
+# on the Peregrine computer cluster
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=install_bbbq
+#SBATCH --output=install_bbbq.log
+module load GCCcore/4.9.3 
+module load XZ/5.2.2-foss-2016a
+#module load R/3.3.1-foss-2016a
+module load R
+Rscript -e 'devtools::install_github("richelbilderbeek/nethmc2pan")'
+Rscript -e 'devtools::install_github("richelbilderbeek/tmhmm")'
+Rscript -e 'devtools::install_github("richelbilderbeek/epitopeome")'
+Rscript -e 'devtools::install_github("richelbilderbeek/bbbq")'
+Rscript -e 'nethmc2pan::install_nethmc2pan()'
+Rscript -e 'tmhmm::install_tmhmm()'
