68c4c41bfcfdbaa1d4b75d35924856c9e525d64e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-02 13:42:18 +0100

    Answer BBBQ

--- R/answer_bbbq.R
+++ R/answer_bbbq.R
@@ -17,5 +17,18 @@ answer_bbbq <- function(
     netmhc2pan_filename = netmhc2pan_filename,
     epitopeome_filename = epitopeome_filename
   )
-  readLines(epitopeome_filename)
+  epitopeome <- readLines(epitopeome_filename)
+  even_indices <- seq_along(epitopeome)
+  even_indices <- even_indices[even_indices %% 2 == 0]
+  amino_acids <- epitopeome[even_indices]
+
+  df <- data.frame(
+    epitopium = c("i", "m", "o", "I", "M", "O"),
+    n = NA,
+    stringsAsFactors = FALSE
+  )
+  for (i in seq(1, nrow(df))) {
+    df$n[i] <- sum(stringr::str_count(amino_acids, df$epitopium[i]))
+  }
+  df
 }
--- tests/testthat/test-answer_bbbq.R
+++ tests/testthat/test-answer_bbbq.R
@@ -5,7 +5,18 @@ test_that("answer_bbbq", {
   testit::assert(netmhc2pan::is_netmhc2pan_installed())
 
   fasta_filename <- system.file("extdata", "short.fasta", package = "bbbq")
-  skip("WIP")
-  df <- answer_bbbq()
+  alleles <- "DRB1_0101"
+  binding_strength_threshold <- 5.0
+  tmhmm_filename <- tempfile(".txt")
+  netmhc2pan_filename <- tempfile(".csv")
+  epitopeome_filename <- tempfile(".fasta")
+  df <- answer_bbbq(
+    fasta_filename = fasta_filename,
+    alleles = alleles,
+    binding_strength_threshold = binding_strength_threshold,
+    tmhmm_filename = tmhmm_filename,
+    netmhc2pan_filename = netmhc2pan_filename,
+    epitopeome_filename = epitopeome_filename
+  )
   expect_true(is.data.frame(df))
 })
