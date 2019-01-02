99c45f46014e2c57443fb4aa2f2cec4aa3c57f3f
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-02 16:37:56 +0100

    Use develop branches [skip ci]

--- scripts/1_install_bbbq.sh
+++ scripts/1_install_bbbq.sh
@@ -13,8 +13,8 @@ module load XZ/5.2.2-foss-2016a
 #module load R/3.3.1-foss-2016a
 module load R
 Rscript -e 'devtools::install_github("richelbilderbeek/netmhc2pan")'
-Rscript -e 'devtools::install_github("richelbilderbeek/tmhmm")'
+Rscript -e 'devtools::install_github("richelbilderbeek/tmhmm", ref = "develop")'
 Rscript -e 'devtools::install_github("richelbilderbeek/epitopeome")'
-Rscript -e 'devtools::install_github("richelbilderbeek/bbbq")'
+Rscript -e 'devtools::install_github("richelbilderbeek/bbbq", ref = "develop")'
 Rscript -e 'netmhc2pan::install_netmhc2pan()'
 Rscript -e 'tmhmm::install_tmhmm()'
