44d5443190e580ca4fc08af2ccd7c8d7ee7ca7f8
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-02 13:25:55 +0100

    Fix doc

--- R/answer_bbbq.R
+++ R/answer_bbbq.R
@@ -1,7 +1,12 @@
 #' Answer the BBBQ
 #' @inheritParams default_params_doc
 answer_bbbq <- function(
-  fasta_filename
+  fasta_filename,
+  alleles = "DRB1_0101",
+  binding_strength_threshold = 5.0,
+  tmhmm_filename = tempfile(".txt"),
+  netmhc2pan_filename = tempfile(".csv"),
+  epitopeome_filename = tempfile(".fasta")
 ) {
   testit::assert(file.exists(fasta_filename))
   epitopeome::create_epitopeome(
