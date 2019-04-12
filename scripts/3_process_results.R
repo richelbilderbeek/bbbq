9ed41414cade0bf52d0853b0e351a2213f932ee2
richelbilderbeek <richel@richelbilderbeek.nl>
2019-04-12 13:33:27 +0200

    Nice figure

--- scripts/3_process_results.R
+++ scripts/3_process_results.R
@@ -74,7 +74,7 @@ ggplot(df, aes(x = allele, y = perc_bound_tmh)) +
   ylab(TeX("$\\frac{M}{I + M + O}$ (%)")) +
   xlab("MHC-II allele") +
   geom_hline(yintercept = 100.0 * f_tmh, linetype = "dashed") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(axis.text.x = element_text(size = 4, angle = 45, hjust = 1)) +
   theme(plot.caption = element_text(hjust = 0)) +
   theme(plot.title = element_text(hjust = 0.5)) + ggsave("figure_1.png")
 
