d7037734a1f0bc29bdde7ecaf5ff4fc12c1ef9ba
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-15 17:05:54 +0200

    EOD

---
+++ R/to_netmhc2pan_name.R
@@ -0,0 +1,16 @@
+#' Convert a Greenbaum et al. name to a NetMHC2pan name
+#' @export
+to_netmhc2pan_name <- function(name) {
+
+  testthat::expect_true(
+    name %in% as.character(bbbq::get_mhc2_haplotype_occurance()$haplotype)
+  )
+
+  result <- stringr::str_replace(name, pattern = "\\*", replacement = "_")
+
+  testthat::expect_true(
+    result %in% netmhc2pan::get_netmhc2pan_alleles()
+  )
+
+  result
+}
