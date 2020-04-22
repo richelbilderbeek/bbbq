2588cbbe7529d2112add6a02fd3b16fa94c37222
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-22 13:18:12 +0200

    Fix doc

--- R/tools.R
+++ R/tools.R
@@ -1,13 +1,20 @@
 #' Explodes a string into a character vector
+#' @param s one string
 #' @author Johannes Textor, Richel J.C. Bilderbeek
+#' @examples
+#' library(testthat)
+#' x <- explode("ABC")
+#' expect_equal(x, c("A", "B", "C"))
 #' @export
 explode <- function(s) {
-  strsplit(s,"")[[1]]
+  strsplit(s, "")[[1]]
 }
 
 #' Version of lapply that also passes along the
 #' name of each list element. f must be a function
 #' with arguments n and x.
+#' @param l list
+#' @param f function
 #' @author Johannes Textor, Richel J.C. Bilderbeek
 #' @export
 nlapply <- function(l, f) {
