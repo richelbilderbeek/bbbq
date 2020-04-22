1948e9990ce82ff6498b2f92d48c19d06cc9f421
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-22 11:57:38 +0200

    Copied code from https://github.com/richelbilderbeek/bianchi_et_al_2017, thanks @jtextor

---
+++ R/tools.R
@@ -0,0 +1,14 @@
+# Orginal code by Johannes Textor
+# Adapted by Richel J.C. Bilderbeek
+
+# explodes a string into a character vector
+explode <- function(s) strsplit(s,"")[[1]]
+
+# version of lapply that also passes along the
+# name of each list element. f must be a function
+# with arguments n and x.
+
+nlapply <- function( l, f ) mapply( f,
+	n=names(l),
+	x=l )
+
