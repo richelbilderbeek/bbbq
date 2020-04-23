e61ef928124a472df996d818c95804d73ec26145
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-23 10:11:15 +0200

    Use Peptides::hydrophobicity

--- R/hydrophobicity-distribution-elution-data.R
+++ R/hydrophobicity-distribution-elution-data.R
@@ -6,24 +6,40 @@
 #'  * non_tmh_eluted_filename
 #'
 #' @inheritParams default_params_doc
+#' @param figure_3_a_filename
+#'   name of the PDF for figure 3a,
+#'   for example `plots/figure-3-a.pdf`
+#' @param figure_3_b_filename
+#'   name of the PDF for figure 3a,
+#'   for example `plots/figure-3-b.pdf`
 #' @author Richel J.C. Bilderbeek, adapted from Johannes Textor
 hydrophobicity_distribution_elution_data <- function(
   kyte_doolittle_scale_as_data_filename,
   tmh_eluted_filename,
-  non_tmh_eluted_filename
+  non_tmh_eluted_filename,
+  figure_3_a_filename,
+  figure_3_b_filename
 ) {
+  testthat::expect_true(file.exists(kyte_doolittle_scale_as_data_filename))
+  testthat::expect_true(file.exists(tmh_eluted_filename))
+  testthat::expect_true(file.exists(non_tmh_eluted_filename))
 
   load(kyte_doolittle_scale_as_data_filename) # Used to be 'load("data/kyte.doolittle.scale.Rdata")'
-  tmh.eluted <- utils::read.table(tmh_eluted_filename, as.is = TRUE)$V1
+  tmh_polypeptides <- utils::read.table(tmh_eluted_filename, as.is = TRUE)$V1
   nontmh.eluted <- utils::read.table(non_tmh_eluted_filename,as.is=TRUE)$V1
 
-  grDevices::pdf("plots/figure-3-a.pdf", width=4, height=4, useDingbats=FALSE)
+  grDevices::pdf(figure_3_a_filename, width=4, height=4, useDingbats=FALSE)
   graphics::par(bty="n", mar=c(4,4,.2,.2))
-
 
   hy <- function(x) mean(kyte.doolittle.scale[bbbq::explode(x)])
 
-  tmh.eluted <- sapply(tmh.eluted,hy)
+  tmh_hydrophobicities <- Peptides::hydrophobicity(tmh_polypeptides)
+  tmh.eluted <- sapply(tmh_polypeptides, hy)
+  testthat::expect_equal(tmh_hydrophobicities, as.numeric(tmh.eluted))
+  # > head(tmh.eluted)
+  # HPRIASGLGL AAIVAYGLYK  ILPLIISKY  APQSFRAEL  APQSFRAEL  GQHVATQHF
+  # 0.3000000  1.1000000  1.5000000 -0.4111111 -0.4111111 -0.6333333
+
   nontmh.eluted <- sapply(nontmh.eluted,hy)
 
   graphics::plot(
@@ -32,7 +48,7 @@ hydrophobicity_distribution_elution_data <- function(
   	ylab = "Probability density",
   	main = "" )
   graphics::lines(
-    stats::density(tmh.eluted),
+    stats::density(tmh_hydrophobicities),
     col = 2
   )
 
@@ -42,7 +58,7 @@ hydrophobicity_distribution_elution_data <- function(
   grDevices::dev.off()
 
 
-  grDevices::pdf("plots/figure-3-b.pdf", width=4, height=4, useDingbats=FALSE)
+  grDevices::pdf(figure_3_b_filename, width=4, height=4, useDingbats=FALSE)
   graphics::par(bty="n", mar=c(4,4,.2,.2))
 
   d <- cbind( c(tmh.eluted,nontmh.eluted), 0 )
---
+++ tests/testthat/test-hydrophobicity_distribution_elution_data.R
@@ -0,0 +1,19 @@
+test_that("use", {
+  skip("Only locally for now")
+
+  kyte_doolittle_scale_as_data_filename <- "~/GitHubs/bianchi_et_al_2017/data/kyte.doolittle.scale.Rdata" # nolint indeed long
+  tmh_eluted_filename <- "~/GitHubs/bianchi_et_al_2017/data/TMH-Bcell-elution.txt" # nolint indeed long
+  non_tmh_eluted_filename <- "~/GitHubs/bianchi_et_al_2017/data/non-TMH-Bcell-elution.txt" # nolint indeed long
+  figure_3_a_filename <- tempfile()
+  figure_3_b_filename <- tempfile()
+
+  hydrophobicity_distribution_elution_data(
+    kyte_doolittle_scale_as_data_filename = kyte_doolittle_scale_as_data_filename,
+    tmh_eluted_filename = tmh_eluted_filename,
+    non_tmh_eluted_filename = non_tmh_eluted_filename,
+    figure_3_a_filename = figure_3_a_filename,
+    figure_3_b_filename = figure_3_b_filename
+  )
+  expect_true(file.exists(figure_3_a_filename))
+  expect_true(file.exists(figure_3_b_filename))
+})
