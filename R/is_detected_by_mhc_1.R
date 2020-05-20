9aa4f069acf6802c874ae9af609349b6cf8c9158
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-20 12:36:58 +0200

    Remove raw data (moved to 'bbbq_article'), allow 'is_detected_by_mhc_1' to supply a haplotype

--- R/is_detected_by_mhc_1.R
+++ R/is_detected_by_mhc_1.R
@@ -1,13 +1,26 @@
 #' Determine if MHC-I has at least one binder to the polypeptide
 #' @inheritParams default_params_doc
 #' @export
-is_detected_by_mhc_1 <- function(protein_sequence) {
+is_detected_by_mhc_1 <- function(
+  protein_sequence,
+  mhc_1_haplotype = "HLA-A-02:01"
+) {
   is_detected <- FALSE
-  tryCatch({
-    EpitopePrediction::binders(protein_sequence)
-    is_detected <- TRUE
-  },
-    error = function(e) {}
+  tryCatch(
+    {
+      EpitopePrediction::binders(
+        x = protein_sequence,
+        mhc = mhc_1_haplotype
+      )
+      is_detected <- TRUE
+    },
+    error = function(e) {
+      if (stringr::str_detect(
+          e$message,
+          pattern = "SMM matrix .* not found"
+        )
+      ) stop(e$message)
+    }
   )
   is_detected
 }
--- tests/testthat/test-is_detected_by_mhc_1.R
+++ tests/testthat/test-is_detected_by_mhc_1.R
@@ -2,3 +2,26 @@ test_that("use", {
   expect_true(is_detected_by_mhc_1("VVIILTIAGNILVIMAVSLE"))
   expect_false(is_detected_by_mhc_1("VVIIRTIAGRILVIMARSLE"))
 })
+
+test_that("detect invalid haplotype", {
+  expect_error(
+    is_detected_by_mhc_1(
+      protein_sequence = "VVIILTIAGNILVIMAVSLE",
+      mhc_1_haplotype = "nonsense"
+    )
+  )
+})
+
+test_that("haplotypes", {
+  skip("WIP")
+  expect_silent(
+    is_detected_by_mhc_1(
+      "VVIILTIAGNILVIMAVSLE",
+      mhc_1_haplotype = get_mhc1_haplotypes()[1]
+    )
+  )
+  EpitopePrediction::binders(
+    x = "VVIILTIAGNILVIMAVSLE",
+    mhc = get_mhc1_haplotypes()[1]
+  )
+})
