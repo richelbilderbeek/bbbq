373aca71ddebecfbce99ea792fe3ec409abe7ddc
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-16 12:50:57 +0200

    Fix doc, install with and without MHCnuggets

--- R/is_detected.R
+++ R/is_detected.R
@@ -1,17 +1,21 @@
-#' Determine if MHC-I or MHC-II has at least one binder to the peptide
+#' Determine if the haplotype has at least one binder to the peptide
 #' @inheritParams default_params_doc
+#' @param percentile how low the IC50 must be for the protein to
+#'   be considered a binder. For example, 0.02 denotes that the protein
+#'   must have an IC50 in the lowest 2% range
 #' @author Richèl J.C. Bilderbeek
 #' @export
 is_detected <- function(
   protein_sequence,
-  mhc_haplotype = "HLA-A-02-01"
+  mhc_haplotype,
+  percentile = 0.02
 ) {
   bbbq::check_mhc_haplotype_name(mhc_haplotype)
 
   ic50_threshold <- mhcnpreds::get_ic50_threshold(
-    n_aas = 9,
+    n_aas = nchar(protein_sequence),
     mhc_haplotype = mhcnuggetsr::to_mhcnuggets_name(mhc_haplotype),
-    percentile = 0.02
+    percentile = percentile
   )
 
   results <- mhcnuggetsr::predict_ic50(
--- tests/testthat/test-is_detected.R
+++ tests/testthat/test-is_detected.R
@@ -1,5 +1,17 @@
 test_that("use", {
-  skip("Not now")
-  expect_true(is_detected("VVIILTIAGNILVIMAVSLE"))
-  expect_false(is_detected("AAAARTIAGRILVIMARSAA"))
+
+  if (!mhcnuggetsr::is_mhcnuggets_installed()) return()
+
+  expect_true(
+    is_detected(
+      "VVIILTIAGN",
+      mhc_haplotype = "HLA-A02:16" # mhcnuggetsr::get_mhc_1_haplotypes()[20]
+    )
+  )
+  expect_false(
+    is_detected(
+      "AAAARTIAGR",
+      mhc_haplotype = "HLA-A02:16" # mhcnuggetsr::get_mhc_1_haplotypes()[20]
+    )
+  )
 })
