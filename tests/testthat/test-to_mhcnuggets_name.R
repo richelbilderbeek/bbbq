9a53a322ce5789a4b6d16025f97637d2e00f0c00
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-16 13:53:41 +0200

    Only test when MHCnuggets is installed

--- tests/testthat/test-to_mhcnuggets_name.R
+++ tests/testthat/test-to_mhcnuggets_name.R
@@ -1,4 +1,5 @@
 test_that("all BBBQ names are proper mhcnuggetsr names", {
+  if (!mhcnuggetsr::is_mhcnuggets_installed()) return()
   for (haplotype_name in bbbq::get_mhc1_haplotypes()) {
     mhcnuggets_name <- mhcnuggetsr::to_mhcnuggets_name(haplotype_name)
     expect_true(mhcnuggets_name %in% mhcnuggetsr::get_mhc_1_haplotypes())
