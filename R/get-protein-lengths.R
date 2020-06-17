3cb905b194e55b13184975f7b65b01e3ac4541c4
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-17 06:55:35 +0200

    Thanks @lintr-bot

--- R/get-protein-lengths.R
+++ R/get-protein-lengths.R
@@ -2,10 +2,10 @@
 #' @author Richèl J.C. Bilderbeek, adapted from Johannes Textor
 get_protein_lengths <- function() {
   x <- seqinr::read.fasta("proteome/UP000005640_9606.fasta.gz",
-  				forceDNAtolower=FALSE, as.string=TRUE )
+  				forceDNAtolower = FALSE, as.string = TRUE)
 
-  for(i in seq_along(x)) {
-  	xn <- strsplit(attr(x[[i]],'name'),"\\|")[[1]][2]
-  	cat(xn," ",nchar(x[[i]]),"\n",sep="")
+  for (i in seq_along(x)) {
+  	xn <- strsplit(attr(x[[i]], 'name'), "\\|")[[1]][2]
+  	cat(xn," ", nchar(x[[i]]), "\n" , sep = "")
   }
 }
