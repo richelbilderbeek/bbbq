4f9ecbe291b972a7efce1dd5ad608e148c664b47
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-23 14:49:57 +0200

    Use as much MHC-I as MHC-II haplotypes, fix #10

--- R/get_mhc2_haplotypes.R
+++ R/get_mhc2_haplotypes.R
@@ -8,27 +8,34 @@
 #' @author Richèl J.C. Bilderbeek
 #' @export
 get_mhc2_haplotypes <- function() {
-  c(
-    "HLA-DRB1*0101",
-    "HLA-DRB1*0301",
-    "HLA-DRB1*0401",
-    "HLA-DRB1*0405",
-    "HLA-DRB1*0701",
-    "HLA-DRB1*0802",
-    "HLA-DRB1*0901",
-    "HLA-DRB1*1101",
-    "HLA-DRB1*1201",
-    "HLA-DRB1*1302",
-    "HLA-DRB1*1501",
-    "HLA-DRB3*0101",
-    "HLA-DRB3*0202",
-    "HLA-DRB4*0101",
-    "HLA-DRB5*0101",
-    "HLA-DQA1*0501/DQB1*0201",
-    "HLA-DQA1*0501/DQB1*0301",
-    "HLA-DQA1*0301/DQB1*0302",
-    "HLA-DQA1*0401/DQB1*0402",
-    "HLA-DQA1*0101/DQB1*0501",
-    "HLA-DQA1*0102/DQB1*0602"
-  )
+  t <- get_mhc2_haplotype_occurance() %>%
+    dplyr::filter(phenotype_freq > 14.0) %>%
+    dplyr::select(haplotype)
+  return(t$haplotype)
+
+  if (1 == 2) {
+      c(
+      "HLA-DRB1*0101",
+      "HLA-DRB1*0301",
+      "HLA-DRB1*0401",
+      "HLA-DRB1*0405",
+      "HLA-DRB1*0701",
+      "HLA-DRB1*0802",
+      "HLA-DRB1*0901",
+      "HLA-DRB1*1101",
+      "HLA-DRB1*1201",
+      "HLA-DRB1*1302",
+      "HLA-DRB1*1501",
+      "HLA-DRB3*0101",
+      "HLA-DRB3*0202",
+      "HLA-DRB4*0101",
+      "HLA-DRB5*0101",
+      "HLA-DQA1*0501/DQB1*0201",
+      "HLA-DQA1*0501/DQB1*0301",
+      "HLA-DQA1*0301/DQB1*0302",
+      "HLA-DQA1*0401/DQB1*0402",
+      "HLA-DQA1*0101/DQB1*0501",
+      "HLA-DQA1*0102/DQB1*0602"
+    )
+  }
 }
--- tests/testthat/test-get_mhc2_haplotypes.R
+++ tests/testthat/test-get_mhc2_haplotypes.R
@@ -2,3 +2,10 @@ test_that("use", {
   expect_silent(get_mhc2_haplotypes())
   expect_true(length(get_mhc2_haplotypes()) > 0)
 })
+
+test_that("matches BBBQ article", {
+  expect_equal(
+    length(get_mhc1_haplotypes()),
+    length(get_mhc2_haplotypes())
+  )
+})
