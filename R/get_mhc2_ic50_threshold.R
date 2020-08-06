a73fc46e15a2d16565a51f5a7b9870fe7d3db07d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-06 13:41:45 +0200

    Deleted obsolete 'get_mhc2_ic50_threshold'

--- R/get_mhc2_ic50_threshold.R
+++
@@ -1,6 +0,0 @@
-#' Get the MHC-II IC50 (in nM) threshold
-#' @author Richèl J.C. Bilderbeek
-#' @export
-get_mhc2_ic50_threshold <- function() {
-  5000.0
-}
--- tests/testthat/test-get_mhc2_ic50_threshold.R
+++
@@ -1,3 +0,0 @@
-test_that("use", {
-  expect_equal(get_mhc2_ic50_threshold(), 5000)
-})
