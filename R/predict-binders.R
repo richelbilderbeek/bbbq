fe5de638aa565b27848beae810ebca0b70d613d4
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-11 10:02:36 +0200

    Follow lintr style

--- R/predict-binders.R
+++ R/predict-binders.R
@@ -9,14 +9,18 @@ predict_binders <- function() {
   x <- seqinr::read.fasta("proteome/UP000005640_9606.fasta.gz",
   		forceDNAtolower = FALSE, as.string = TRUE )
 
-  for(i in seq_along(x) ){
+  for (i in seq_along(x)) {
   	tryCatch({
   		xn <- strsplit(attr(x[[i]],'name'),"\\|")[[1]][2]
 
   		# RJCB: So, the best binder is defined per protein
-  		xb <- EpitopePrediction::binders( x[[i]], mhc, quantile.threshold = 0.02)
-  		utils::write.table( cbind( xn, xb[,1:3] ), col.names = FALSE, row.names = FALSE, quote = FALSE)
-  		 }, error = function(e){} )
+  		xb <- EpitopePrediction::binders(x[[i]], mhc, quantile.threshold = 0.02)
+  		utils::write.table(
+  		  cbind(xn, xb[, 1:3]),
+  		  col.names = FALSE, row.names = FALSE, quote = FALSE)
+  	  },
+  	  error = function(e) {} # nolint indeed do nothing
+    )
   }
 }
 
