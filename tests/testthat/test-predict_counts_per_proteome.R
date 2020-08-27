411d8dd85a8ea1cab557e1d39df3e64090511b73
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-27 13:30:20 +0200

    Expose https://github.com/richelbilderbeek/bbbq_article/issues/58

--- tests/testthat/test-predict_counts_per_proteome.R
+++ tests/testthat/test-predict_counts_per_proteome.R
@@ -63,6 +63,30 @@ test_that("MHCnuggetsr, MHC-II, peptide length 13", {
   expect_true("n_spots_tmh" %in% names(t))
 })
 
+test_that("MHCnuggetsr on short sequence", {
+  skip("WIP, https://github.com/richelbilderbeek/bbbq_article/issues/58")
+
+  if (!pureseqtmr::is_pureseqtm_installed()) return()
+  if (!mhcnuggetsr::is_mhcnuggets_installed()) return()
+
+  protein_sequences <- c(
+    "FANTASTICALLY",
+    "GTGG"
+  )
+  t <- predict_counts_per_proteome(
+    protein_sequences = protein_sequences,
+    haplotype = get_mhc2_haplotypes()[1],
+    peptide_length = 13,
+    percentile = 0.05
+  )
+  expect_true(tibble::is_tibble(t))
+  expect_equal(nrow(t), length(protein_sequences))
+  expect_true(is.na(t$n_binders[2]))
+  expect_true(is.na(t$n_binders_tmh[2]))
+  expect_true(is.na(t$n_spots[2]))
+  expect_true(is.na(t$n_spots_tmh[2]))
+})
+
 test_that("use, EpitopePrediction, MHC-I, peptide length 9", {
   if (!pureseqtmr::is_pureseqtm_installed()) return()
 
@@ -108,6 +132,31 @@ test_that("EpitopePrediction does not support U", {
   t
 })
 
+test_that("EpitopePrediction on short peptide", {
+  skip("WIP, https://github.com/richelbilderbeek/bbbq_article/issues/58")
+  if (!pureseqtmr::is_pureseqtm_installed()) return()
+
+  # https://github.com/richelbilderbeek/bbbq_article/issues/58
+  protein_sequences <- c(
+    "FANTASTICALLY",
+    "GTGG"
+  )
+  t <- predict_counts_per_proteome(
+    protein_sequences = protein_sequences,
+    haplotype = get_mhc1_haplotypes()[1],
+    peptide_length = 9,
+    percentile = 0.05,
+    ic50_prediction_tool = "EpitopePrediction"
+  )
+  expect_true(tibble::is_tibble(t))
+  expect_equal(nrow(t), length(protein_sequences))
+  expect_true(is.na(t$n_binders[2]))
+  expect_true(is.na(t$n_binders_tmh[2]))
+  expect_true(is.na(t$n_spots[2]))
+  expect_true(is.na(t$n_spots_tmh[2]))
+  t
+})
+
 test_that("use, NetMHC2pan, MHC-II, peptide length 13", {
   if (!pureseqtmr::is_pureseqtm_installed()) return()
   if (!netmhc2pan::is_netmhc2pan_installed()) return()
@@ -129,4 +178,54 @@ test_that("use, NetMHC2pan, MHC-II, peptide length 13", {
   expect_true("n_binders_tmh" %in% names(t))
   expect_true("n_spots" %in% names(t))
   expect_true("n_spots_tmh" %in% names(t))
+})
+
+test_that("NetMHC2pan does handle U", {
+  if (!pureseqtmr::is_pureseqtm_installed()) return()
+  if (!netmhc2pan::is_netmhc2pan_installed()) return()
+
+  protein_sequences <- c(
+    "FANTASTICALLY",
+    "FANTASTICALLU"
+  )
+  t <- predict_counts_per_proteome(
+    protein_sequences = protein_sequences,
+    haplotype = get_mhc2_haplotypes()[1],
+    peptide_length = 13,
+    percentile = 0.05,
+    ic50_prediction_tool = "netmhc2pan"
+  )
+  t
+  expect_true(tibble::is_tibble(t))
+  expect_equal(nrow(t), length(protein_sequences))
+  expect_true("n_binders" %in% names(t))
+  expect_true("n_binders_tmh" %in% names(t))
+  expect_true("n_spots" %in% names(t))
+  expect_true("n_spots_tmh" %in% names(t))
+})
+
+test_that("NetMHC2pan on short peptide", {
+  skip("WIP, https://github.com/richelbilderbeek/bbbq_article/issues/58")
+
+  if (!pureseqtmr::is_pureseqtm_installed()) return()
+  if (!netmhc2pan::is_netmhc2pan_installed()) return()
+
+  protein_sequences <- c(
+    "FANTASTICALLY",
+    "GTGG"
+  )
+  t <- predict_counts_per_proteome(
+    protein_sequences = protein_sequences,
+    haplotype = get_mhc2_haplotypes()[1],
+    peptide_length = 13,
+    percentile = 0.05,
+    ic50_prediction_tool = "netmhc2pan"
+  )
+  t
+  expect_true(tibble::is_tibble(t))
+  expect_equal(nrow(t), length(protein_sequences))
+  expect_true(is.na(t$n_binders[2]))
+  expect_true(is.na(t$n_binders_tmh[2]))
+  expect_true(is.na(t$n_spots[2]))
+  expect_true(is.na(t$n_spots_tmh[2]))
 })
