b6a22c912292fc01026d4868baaae444094374d9
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-28 10:03:01 +0200

    Add 'create_consensus_topology_conservation'

---
+++ R/create_consensus_topology_conservation.R
@@ -0,0 +1,37 @@
+#' Create a consensus-topology-conservation table
+#'
+#' @return a \link{tibble} with columns:\cr
+#' \itemize{
+#'   \item aa the amino acid at that position in consensus sequence
+#'   \item score the conservation score
+#'   \item is_tmh if that amino acid at that location is part of a
+#'     transmembrane helix
+#' }
+#' @inheritParams default_params_doc
+#' @export
+create_consensus_topology_conservation <- function( # nolint indeed a long function name
+  protein_sequences
+) {
+  protein_sequences_aass <- Biostrings::AAStringSet(protein_sequences)
+
+  protein_alignment <- msa::msa(protein_sequences_aass)
+
+  data(BLOSUM62)
+  conservation_scores <- msa::msaConservationScore(protein_alignment, BLOSUM62)
+  t <- tibble::tibble(
+    aa = names(conservation_scores),
+    score = as.numeric(conservation_scores),
+    is_tmh = NA
+  )
+  consensus <- paste0(names(conservation_scores), collapse = "")
+  topology <- pureseqtmr::predict_topology_from_sequence(consensus)
+
+  topology_index <- 1
+  for (row_index in seq(1, nrow(t))) {
+    if (t$aa[row_index] != "-") {
+      t$is_tmh[row_index] <- stringr::str_sub(topology, topology_index, topology_index)
+      topology_index <- topology_index + 1
+    }
+  }
+  t
+}
---
+++ tests/testthat/test-create_alignment_with_topology.R
@@ -0,0 +1,47 @@
+test_that("use, simple data", {
+
+  protein_sequences <- c(
+    "IMPRESSIVELYFLIFAWAYFANSWALKSWEETMARKETTRIVIALLYNAILIDENTIFY",
+     "MPRESSIVELYFLIAAWAYFANSWALKSWEETMARKETTRIVIALLYNAILIDENTIFY",
+     "IMPRESSIVELYFLMTAWAYFANSWALKSWEETMARKETTRIVIALLYNAILIDENTIF",
+    "IMPRESSIVELYFLIIAWAYANSWALKSWEETMARKETTRIVIALLYNAILIDENTIFY",
+    "IMPRESSIVELYFLILAWAYFANSWALKWEETMARKETTRIVIALLYNAILIDENTIFY",
+    "IMPRESSIVELYFLIYAWAYFANSWALKSWEETMARKETRIVIALLYNAILIDENTIFY"
+  )
+  t <- create_consensus_topology_conservation(protein_sequences)
+  expect_true(tibble::is_tibble(t))
+  expect_equal(60, nrow(t))
+  if (1 == 2) {
+    plot(t$score)
+    ggplot2::ggplot(t, ggplot2::aes(x = as.factor(is_tmh), y = score)) +
+      ggplot2::geom_boxplot()
+  }
+})
+
+test_that("use, simple data", {
+
+  skip("WIP")
+  proteomes_filename <- "~/GitHubs/bbbq_article/bbbq_2/allprot0621.fasta"
+  expect_true(file.exists(proteomes_filename))
+  proteome <- seqinr::read.fasta(proteomes_filename, seqtype = "AA", as.string = TRUE)
+
+  # Select the envelope proteins
+  evelope_sequences <- proteome[stringr::str_which(names(proteome), "^E\\|")]
+
+  # Select the unique ones
+  unique_evelope_sequences <- evelope_sequences[
+    !duplicated(seqinr::getSequence(evelope_sequences))
+  ]
+  protein_sequences <- unlist(seqinr::getSequence(unique_evelope_sequences, as.string = TRUE))
+
+  t <- create_consensus_topology_conservation(protein_sequences)
+  expect_true(tibble::is_tibble(t))
+  expect_equal(123, nrow(t))
+  if (1 == 2) {
+    plot(t$score)
+
+    ggplot2::ggplot(na.omit(t), ggplot2::aes(x = as.factor(is_tmh), y = score)) +
+      ggplot2::geom_boxplot() +
+      ggplot2::scale_y_continuous(limits = c(0, max(t$score)))
+  }
+})
