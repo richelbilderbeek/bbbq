cb14f260cf2e6c107435fa5b6399ab36cd7ebe56
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-23 12:27:05 +0200

    Write tests 'get_uniprot_id', progress https://github.com/richelbilderbeek/bbbq_article/issues/116

---
+++ tests/testthat/test-get_uniprot_id.R
@@ -0,0 +1,12 @@
+test_that("use", {
+  skip("Progress https://github.com/richelbilderbeek/bbbq_article/issues/116")
+  expect_equal(get_uniprot_id("covid"), "UP000464024")
+  expect_equal(get_uniprot_id("flua"), "UP000009255")
+  expect_equal(get_uniprot_id("hepa"), "UP000006724")
+  expect_equal(get_uniprot_id("hiv"), "UP000002241")
+  expect_equal(get_uniprot_id("human"), "UP000005640")
+  expect_equal(get_uniprot_id("polio"), "UP000000356")
+  expect_equal(get_uniprot_id("myco"), "UP000001584")
+  expect_equal(get_uniprot_id("rhino"), "UP000007070")
+  expect_error(get_uniprot_id("nonsense"), "Unknown target 'nonsense'")
+})
