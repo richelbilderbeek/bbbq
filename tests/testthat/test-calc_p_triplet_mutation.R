f87691e5db56f619ace8ae4cace7a44452c68b0a
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-29 10:10:38 +0100

    Delete test for code that will never be written

--- tests/testthat/test-calc_p_triplet_mutation.R
+++
@@ -1,8 +0,0 @@
-test_that("use", {
-  skip("Not needed")
-  p_one_mut <- calc_p_triplet_mutation("AAA", "AAC")
-  p_two_muts <- calc_p_triplet_mutation("AAA", "ACC")
-  p_three_muts <- calc_p_triplet_mutation("AAA", "CCC")
-  expect_equal(p_one_mut ^ 2, p_two_mut)
-  expect_equal(p_one_mut ^ 3, p_three_mut)
-})
