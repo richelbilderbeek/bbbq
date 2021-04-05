bf75167facc902e65e7a067a0f24fc1739034792
richelbilderbeek <richel@richelbilderbeek.nl>
2021-04-05 10:08:33 +0200

    Use a default size

--- R/get_bbbq_theme.R
+++ R/get_bbbq_theme.R
@@ -2,12 +2,17 @@
 #' @export
 get_bbbq_theme <- function() {
   ggplot2::theme_bw() +
-  ggplot2::theme(axis.line = ggplot2::element_line(colour = "black"),
+  ggplot2::theme(
+    axis.line = ggplot2::element_line(colour = "black"),
+    legend.key = ggplot2::element_blank(),
     panel.grid.major = ggplot2::element_blank(),
     panel.grid.minor = ggplot2::element_blank(),
     panel.border = ggplot2::element_blank(),
     panel.background = ggplot2::element_blank(),
-    legend.key = ggplot2::element_blank(),
-    strip.background = ggplot2::element_rect(colour = "white", fill = "#FFFFFF")
+    strip.background = ggplot2::element_rect(
+      colour = "white",
+      fill = "#FFFFFF"
+    ),
+    text = ggplot2::element_text(size = 24)
   )
 }
